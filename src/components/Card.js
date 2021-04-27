b852df2e3de292eb34f2a001e869f2b2077953d8
Carlos Alvarez <car.alv2804@gmail.com>
2021-04-27 15:03:08 -0500

    Modificando tarjetas

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,22 +4,17 @@ import PropTypes from 'prop-types';
 function Card({title, image, text, url}) {
 
     return (
-        <div className="card text-center bg-dark animate__animated animate__fadeInUp">
+        <div className="card text-center bg-light animate__animated animate__fadeInUp">
             <div className="overflow">
                 <img src={image} alt={title} className="card-img-top" />
             </div>
             <div className="card-body text-light">
-                <h4 className="card-title">{title}</h4>
-                <p className="card-text text-secondary">
-                    {
-                        text ? text : 'Falta información de la tarjeta'
-                    }
-                </p>
+                
                 <a 
                     href={url} 
                     target="_blank" 
                     rel="noreferrer"
-                    className="btn btn-outline-secondary rounded-0"
+                    className="btn btn-outline-dark"
                 >
                     Go to this website
                 </a>
